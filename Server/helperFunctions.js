a38aab9cd51c0d4686a1eecc293e7c4765f66d4d
GH0ST555 <110325018+GH0ST555@users.noreply.github.com>
2024-04-14 22:30:08 +0400

    Finished all test cases for all non MM functions.

--- Server/helperFunctions.js
+++ Server/helperFunctions.js
@@ -7,7 +7,7 @@ export function getNeighbors(nodeId, rows, cols) {
 
     // Check if nodeId, rows, and cols are positive integers
     if (!Number.isInteger(nodeId) || nodeId < 0 || !Number.isInteger(rows) || rows <= 0 || !Number.isInteger(cols) || cols <= 0) {
-        return neighbors;
+      throw new Error('nodeId, rows, and cols must be positive integers');
     }
     const row = Math.floor(nodeId / cols);
     const col = nodeId % cols;
@@ -27,44 +27,55 @@ export function getNeighbors(nodeId, rows, cols) {
    }
    
   export function getConnectedLinks(nodeId, links) {
+    if (!Number.isInteger(nodeId) || nodeId < 0 ) {
+      throw new Error('nodeId must be a positive integer');
+    }
      // Filter links where the current nodeId is either the source or the target
      const connectedLinks = links.filter(link => link.source === nodeId || link.target === nodeId);  
      return connectedLinks;
    }
    
    
-   
+    // Function to get row and col for a node ID
+    export const getRowCol = (nodeId, cols) => {
+      const row = Math.floor(nodeId / cols);
+      const col = nodeId % cols;
+      return { row, col };
+    };
+
    // Calculate relative positions (When the origin is a node)
    export const calculatePositionsNC = (originNode, nodes, links, rows, cols) => {
-     const { row: originRow, col: originCol } = getRowCol(originNode, cols);
-     // Calculate relative positions for nodes
-     const rpNodes = nodes.map(node => {
-       const { row, col } = getRowCol(node, cols);
-       return {
-         nodeId: node,
-         relativePosition: { x: col - originCol, y: row - originRow }
-       };
-     });
-   
-     // Calculate relative positions for links
-     const rpLinks = links.map(link => {
-       const { row: sourceRow, col: sourceCol } = getRowCol(link.source, cols);
-       const { row: targetRow, col: targetCol } = getRowCol(link.target, cols);
-       return {
-         linkId: link.id,
-         sourceRelativePosition: { x: sourceCol - originCol, y: sourceRow - originRow },
-         targetRelativePosition: { x: targetCol - originCol, y: targetRow - originRow },
-         edgetype: link.edgetype
-       };
-     });
+    // Check if nodeId, rows, and cols are positive integers
+    if (!Number.isInteger(originNode) || originNode < 0 || !Number.isInteger(rows) || rows <= 0 || !Number.isInteger(cols) || cols <= 0) {
+      throw new Error('origin node, rows, and cols must be positive integers');
+    }
+
+    const { row: originRow, col: originCol } = getRowCol(originNode, cols);
+
+    // Calculate relative positions for nodes
+    const rpNodes = nodes.map(node => {
+      const { row, col } = getRowCol(node.id, cols);
+
+      return {
+        nodeId: node,
+        relativePosition: { x: col - originCol, y: row - originRow }
+      };
+    });
+  
+    // Calculate relative positions for edges
+    const rpLinks = links.map(link => {
+      const { row: sourceRow, col: sourceCol } = getRowCol(link.source, cols);
+      const { row: targetRow, col: targetCol } = getRowCol(link.target, cols);
+      return {
+        linkId: link.id,
+        sourceRelativePosition: { x: sourceCol - originCol, y: sourceRow - originRow },
+        targetRelativePosition: { x: targetCol - originCol, y: targetRow - originRow },
+        edgetype: link.edgetype
+      };
+    });
    
      return { rpNodes, rpLinks };
    };
    
-   // Function to get row and col for a node ID
-   export const getRowCol = (nodeId, cols) => {
-     const row = Math.floor(nodeId / cols);
-     const col = nodeId % cols;
-     return { row, col };
-   };
+
    
--- Server/helperFunctions.test.js
+++ Server/helperFunctions.test.js
@@ -1,8 +1,8 @@
-import { getNeighbors,getConnectedLinks,calculatePositionsNC } from "./helperFunctions";
+import { getNeighbors,getConnectedLinks,calculatePositionsNC,getRowCol } from "./helperFunctions";
 import { createAdjacencyMatrix, createNodesandEdges } from './CreateDS.js';
 
 describe('getNeighbours', () => {
-    test('gets all possible neighbours  when selected node has 4 neighbours)', () => {
+    test('gets all possible neighbours  when selected node has 4 neighbours', () => {
       const rows = 3;
       const cols = 3;
       const expected_neighbours = [3,5,1,7];
@@ -37,22 +37,17 @@ describe('getNeighbours', () => {
       test('handling invalid neighbour ID s', () => {
         const rows = 1;
         const cols = 2;
-        const expected_neighbours = [];
-        const neighbours_invalid_int = getNeighbors(-1,rows, cols);
-        expect(neighbours_invalid_int).toEqual(expected_neighbours);
+        expect(()=> getNeighbors(-1,rows, cols)).toThrow(/nodeId, rows, and cols must be positive integers/);
 
-        //whewn the string 
-        const neighbours_invalid_str = getNeighbors('aab',rows, cols);
-        expect(neighbours_invalid_str).toEqual(expected_neighbours);
+        //whewn the string is passed
+        expect(()=> getNeighbors('aab',rows, cols)).toThrow(/nodeId, rows, and cols must be positive integers/);
       });
 
       test('handling invalid row and column ID s', () => {
         //One value is less than 0 and one is a string
         const rows = -1;
         const cols = 'aab';
-        const expected_neighbours = [];
-        const neighbours_invalid_int = getNeighbors(-1,rows, cols);
-        expect(neighbours_invalid_int).toEqual(expected_neighbours);
+        expect(()=> getNeighbors(-1,rows, cols)).toThrow(/nodeId, rows, and cols must be positive integers/);
 
       });
 });
@@ -60,15 +55,173 @@ describe('getNeighbours', () => {
 
 
 describe('getConnectedLinks', () => {
-    test('gets all possible edges  when selected node has 4 neighbours)', () => {
+    test('gets all possible edges  when selected node has 4 neighbours', () => {
       const rows = 3;
       const cols = 3;
 
       const matrix = createAdjacencyMatrix(rows, cols);
       const { nodes, links } = createNodesandEdges(matrix, rows, cols);
 
+      //pass the node id to find its neighbouring edges
       const actual_links = getConnectedLinks(4,links);
+
       expect(actual_links.length).toBe(8);
+      actual_links.forEach(link => {
+        //Source or target node must be 4 when getting all possible edges connected to node id 4
+        expect([link.source, link.target]).toContain(4);
+      });
     });
 
+    test('gets all possible edges  when selected node has 3 neighbours', () => {
+      const rows = 2;
+      const cols = 3;
+
+      const matrix = createAdjacencyMatrix(rows, cols);
+      const { nodes, links } = createNodesandEdges(matrix, rows, cols);
+
+      //pass the node id to find its neighbouring edges
+      const actual_links = getConnectedLinks(4,links);
+
+      expect(actual_links.length).toBe(6);
+      actual_links.forEach(link => {
+        //Source or target node must be 4 when getting all possible edges connected to node id 4
+        expect([link.source, link.target]).toContain(4);
+      });
+    });
+
+    test('gets all possible edges  when selected node has 2 neighbours', () => {
+      const rows = 3;
+      const cols = 3;
+
+      const matrix = createAdjacencyMatrix(rows, cols);
+      const { nodes, links } = createNodesandEdges(matrix, rows, cols);
+
+      //pass the node id to find its neighbouring edges
+      const actual_links = getConnectedLinks(0,links);
+
+      expect(actual_links.length).toBe(4);
+      actual_links.forEach(link => {
+        //Source or target node must be 4 when getting all possible edges connected to node id 4
+        expect([link.source, link.target]).toContain(0);
+      });
+    });
+    
+    test('gets all possible edges  when selected node has 1 neighbour', () => {
+      const rows = 1;
+      const cols = 2;
+
+      const matrix = createAdjacencyMatrix(rows, cols);
+      const { nodes, links } = createNodesandEdges(matrix, rows, cols);
+
+      //pass the node id to find its neighbouring edges
+      const actual_links = getConnectedLinks(0,links);
+
+      expect(actual_links.length).toBe(2);
+      actual_links.forEach(link => {
+        //Source or target node must be 4 when getting all possible edges connected to node id 4
+        expect([link.source, link.target]).toContain(0);
+      });
+
+    });
+
+    test('handling invalid neighbour ID s', () => {
+      const rows = 1;
+      const cols = 2;
+
+      const matrix = createAdjacencyMatrix(rows, cols);
+      const { nodes, links } = createNodesandEdges(matrix, rows, cols);
+
+      //when a number below 0 is passed as node id an error message should be thrown
+      expect(()=> getConnectedLinks(-1,links)).toThrow(/nodeId must be a positive integer/);
+
+      //when the string is passed as node id an error message should be thrown
+      expect(()=> getConnectedLinks('aab',links)).toThrow(/nodeId must be a positive integer/);
+    });
+
+});
+
+
+describe('calculatePositionsNC', () => {
+  test('Test if relative position code is accurate', () => {
+    const rows = 2;
+    const cols = 2;
+
+    const matrix = createAdjacencyMatrix(rows, cols);
+    const { nodes, links } = createNodesandEdges(matrix, rows, cols);
+
+    const {rpNodes,rpLinks} = calculatePositionsNC(0, nodes, links, rows, cols);
+
+
+    expect(rpNodes).toEqual([
+      { nodeId: { id: 0 }, relativePosition: { x: 0, y: 0 } },
+      { nodeId: { id: 1 }, relativePosition: { x: 1, y: 0 } },
+      { nodeId: { id: 2 }, relativePosition: { x: 0, y: 1 } },
+      { nodeId: { id: 3 }, relativePosition: { x: 1, y: 1 } }
+    ]);
+
+    expect(rpLinks).toEqual([
+      {
+        linkId: 'link-0-1',
+        sourceRelativePosition: { x: 0, y: 0 },
+        targetRelativePosition: { x: 1, y: 0 },
+        edgetype: 'Horizontal'
+      },
+      {
+        linkId: 'link-0-2',
+        sourceRelativePosition: { x: 0, y: 0 },
+        targetRelativePosition: { x: 0, y: 1 },
+        edgetype: 'Vertical'
+      },
+      {
+        linkId: 'link-1-0',
+        sourceRelativePosition: { x: 1, y: 0 },
+        targetRelativePosition: { x: 0, y: 0 },
+        edgetype: 'Horizontal'
+      },
+      {
+        linkId: 'link-1-3',
+        sourceRelativePosition: { x: 1, y: 0 },
+        targetRelativePosition: { x: 1, y: 1 },
+        edgetype: 'Vertical'
+      },
+      {
+        linkId: 'link-2-0',
+        sourceRelativePosition: { x: 0, y: 1 },
+        targetRelativePosition: { x: 0, y: 0 },
+        edgetype: 'Vertical'
+      },
+      {
+        linkId: 'link-2-3',
+        sourceRelativePosition: { x: 0, y: 1 },
+        targetRelativePosition: { x: 1, y: 1 },
+        edgetype: 'Horizontal'
+      },
+      {
+        linkId: 'link-3-1',
+        sourceRelativePosition: { x: 1, y: 1 },
+        targetRelativePosition: { x: 1, y: 0 },
+        edgetype: 'Vertical'
+      },
+      {
+        linkId: 'link-3-2',
+        sourceRelativePosition: { x: 1, y: 1 },
+        targetRelativePosition: { x: 0, y: 1 },
+        edgetype: 'Horizontal'
+      }
+    ]);
+  });
+
+  test('Test for improper values', () => {
+    const rows = 2;
+    const cols = 2;
+
+    const matrix = createAdjacencyMatrix(rows, cols);
+    const { nodes, links } = createNodesandEdges(matrix, rows, cols);
+
+    //when a number below 0 is passed as node id an error message should be thrown
+    expect(()=> calculatePositionsNC(-1, nodes, links, rows, cols)).toThrow(/origin node, rows, and cols must be positive integers/);
+
+    //when the string is passed as node id an error message should be thrown
+    expect(()=> calculatePositionsNC('aab', nodes, links, rows, cols)).toThrow(/origin node, rows, and cols must be positive integers/);
+  });
 });
